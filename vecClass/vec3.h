8015fd27ac4b5ad0f7919983ff9969777983e1ab
RedHong56 <redhong9956@gmail.com>
2026-09-23 18:29:00 +0900

    feat(vector3): utilty operate funtion
    - 벡터들 연산 함수

--- vecClass/vec3.h
+++ vecClass/vec3.h
@@ -54,4 +54,40 @@ struct Vector3
 typedef Vector3 Vec3;
 using Point3 = Vector3; // alias for 3D point
 
+// Utility functions
+inline std::ostream& operator<<(std::ostream& out, const Vector3& v)
+{
+	return out << v.E[0] << ' ' << v.E[1] << ' ' << v.E[2];
+}
+
+inline Vector3 operator+(const Vector3& u, const Vector3& v)
+{
+	return Vector3(u.E[0] + v.E[0], u.E[1] + v.E[1], u.E[2] + v.E[2]);
+}
+
+inline Vector3 operator-(const Vector3& u, const Vector3& v)
+{
+	return Vector3(u.E[0] - v.E[0], u.E[1] - v.E[1], u.E[2] - v.E[2]);
+}
+
+inline Vector3 operator*(const Vector3& u, const Vector3& v)
+{
+	return Vector3(u.E[0] * v.E[0], u.E[1] * v.E[1], u.E[2] * v.E[2]);
+}
+
+inline Vector3 operator*(double t, const Vector3& v)
+{
+	return Vector3(t * v.E[0], t * v.E[1], t * v.E[2]);
+}
+
+inline Vector3 operator*(const Vector3& v, double t)
+{
+	return t * v;
+}
+
+inline Vector3 operator/(Vector3 v, double t)
+{
+	return (1 / t) * v;
+}
+
 #endif // VEC3_H
